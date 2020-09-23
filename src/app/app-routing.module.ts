389bbdf7e58abfa38296afc81e46582ce3d303d9
Jairo Tabares <jairotabares@gmail.com>
2020-09-23 17:42:39 -0500

    Versión inicial plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,66 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { EntryComponent } from './entry/entry.component';
+import { StuffComponent } from './stuff/stuff.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'entry',
+    component: EntryComponent,
+  },
+  {
+    path: 'app',
+    component: AppComponent,
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'reservation',
+    component: ReservationComponent,
+  },
+  {
+    path: 'stuff',
+    component: StuffComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'blogdetails',
+    component: BlogdetailsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'app', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'app', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { EntryComponent } from './entry/entry.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StuffComponent } from './stuff/stuff.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    ReservationComponent,
+    EntryComponent,
+    HeaderComponent,
+    FooterComponent,
+    StuffComponent,
+    GalleryComponent,
+    BlogComponent,
+    BlogdetailsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EntryComponent]
 })
 export class AppModule { }
