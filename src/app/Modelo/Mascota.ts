92296ee40715254e6e7f8ea50a4304005c12110b
Andres Camilo Diaz Mora <ancdiazmo@gmail.com>
2020-10-15 11:06:27 -0500

    se crea el listar de mascotas y el caso de uso de guardar una mascota, aun falta hacer el editar y que la lista se cargue automticamente al hacer el cambio

---
+++ src/app/Modelo/Mascota.ts
@@ -0,0 +1,10 @@
+import { Usuario } from './Usuario';
+
+export class Mascota {
+    id: number;
+    especie: string;
+    raza: string;
+    nombre: string;
+    edad: string;
+    usuario: Usuario;
+}
