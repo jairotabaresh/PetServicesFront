34d12eb1802c8aced8b90077336930b6bb6337ac
Jairo Tabares <jairotabares@gmail.com>
2020-10-15 11:46:30 -0500

    commit para manejo de correo

---
+++ src/app/Service/correo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Correo } from '../Modelo/Correo';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CorreoService {
+
+  private ruta = environment.baseUrl + '/contactoController';
+  constructor(private http: HttpClient) { }
+
+  public Enviar(correo: Correo){
+
+    return this.http.post<boolean>(this.ruta + '/enviar', correo);
+  }
+}
